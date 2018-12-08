5719047efce80ad073138f827d905cba6ec2a1d0
Amulya Pervaje <akpervaj@ncsu.edu>
2018-12-08 08:09:08 -0500

    alpha1iPP.py working on getting bonds right

---
+++ alpha1iPP.py
@@ -0,0 +1,126 @@
+import numpy as np
+import makesemicrystallineLmpsfEMC as msc
+import math
+
+
+def writexyz(xyzfile, atomsinfo):
+    Natoms = len(atomsinfo)
+    fout = open(xyzfile, 'w')
+    fout.write(str(Natoms) + "\nAtoms\n")
+    for i in range(Natoms):
+        fout.write(str(int(atomsinfo[i,0])) + " " + str(int(atomsinfo[i,2])) + " " + str(atomsinfo[i,3]) + " " + str(atomsinfo[i,4]) + " " + str(atomsinfo[i,5]) + "\n")
+    fout.close()
+
+def setupInfiniteSystem(xyzfile, Rx, Ry, Rz):
+    atomsinfo = []
+    a = 6.63 
+    b = 20.78
+    c = 6.50
+    atype = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    beta = 99.5 
+    diffangle = 99.5 - 90
+    #find unit vector of new c axis
+    zoffset = np.cos(math.radians(diffangle))
+    xoffset = -np.sin(math.radians(diffangle))
+    print("xoffset", xoffset, "zoffset", zoffset)
+    rcoords = np.zeros((9,3))
+    rcoords[:,0] = [float(i) for i in "-0.0727 -0.0765 -0.1021 -0.3087 -0.1146 -0.1044 0.2775 0.0872 0.1026".split()]
+    rcoords[:,1] = [float(i) for i in "0.2291 0.1592 0.1602 0.0589 0.0928 0.0854 0.0797 0.1156 0.1221".split()]
+    rcoords[:,2] = [float(i) for i in "0.2004 0.2788 0.5098 0.4941 0.6057 0.8428 0.9260 0.9730 1.2109".split()]
+    #rcoords = np.zeros((8,3))
+
+    #rcoords[:,0] = [0, 0, 0.5, 0.5, 0, 0, 0.5, 0.5]
+    #rcoords[:,1] = [0, 0.5, 0, 0.5, 0, 0.5, 0, 0.5]
+    #rcoords[:,2] = [0, 0, 0, 0, 0.5, 0.5, 0.5, 0.5 ]
+    #rcoords = np.array([[0,0,0]])
+    symmetries = np.array([[1,1,1], [-1,-1,-1], [1,-1+0.5, 1+0.5], [-1, 1+0.5, -1+0.5]])
+    count = 1
+    mol = 1
+    for k in range(Rz):
+        basez =k*c*zoffset
+        for j in range(Ry):
+            basey = j*b
+            for i in range(Rx):
+                basex = i*a + xoffset*k*c*a 
+                for s in range(len(symmetries)):
+                    mol = 1 + s + k*len(symmetries)
+                    for ci in range(len(rcoords)):
+                        x = basex + (symmetries[s][0]*a + rcoords[ci][0]*a) + xoffset*(symmetries[s][2]*c + rcoords[ci][2]*c)*a
+                        y = basey + symmetries[s][1]*b + rcoords[ci][1]*b
+                        z = basez + (symmetries[s][2]*c + rcoords[ci][2]*c)*zoffset 
+                        atomsinfo.append([count, mol, atype[ci], x, y, z])
+                        count = count + 1
+                    
+
+    atomsinfo = np.array(atomsinfo)
+    writexyz(xyzfile, atomsinfo)
+    #xlo = -(Rx+1)*a
+    #ylo = -(Ry+1)*b
+    #zlo = -(Rz+1)*c
+    #xhi =  (Rx+1)*a
+    #yhi = (Ry+1)*b*(1.5)
+    #zhi = (Rz+1)*c*(1.5)
+    xlo = min(atomsinfo[:,3]) - 5
+    xhi = max(atomsinfo[:,3]) + 5
+    ylo = min(atomsinfo[:,4]) - 5
+    yhi = max(atomsinfo[:,4]) + 5
+    zlo = min(atomsinfo[:,5]) - 5
+    zhi = max(atomsinfo[:,5]) + 5
+    return atomsinfo, xlo, xhi, ylo, yhi, zlo, zhi
+
+def readlammpsbondsPPctypes(filename,newfile):
+    Cmasstypes = [13.019, 14.027, 15.035]
+    boxcoords, masstypes, atoms, bonds, angles, dihedrals, atomsinfo, bondsinfo, anglesinfo, dihedralsinfo = msc.readlammpsdata(filename)
+    #change atomtypes in based on number of bonds
+    #loop through bondsinfo
+    numBonds = np.zeros(len(atomsinfo))
+    for i in range(len(bondsinfo)):
+        bond = bondsinfo[i,:]
+        atom1 = bond[2]-1
+        atom2 = bond[3]-1
+        numBonds[int(atom1)] += 1
+        numBonds[int(atom2)] += 1
+
+    #update atomsinfo
+    for i in range(len(atomsinfo)):
+        atomsinfo[i,2] = numBonds[i]
+
+    #write new lammps data with just atoms 
+    msc.writelammpsdatajustatoms(newfile, boxcoords, Cmasstypes, len(atomsinfo), atomsinfo)
+        
+
+def readxyz(filename):
+    count = 0 
+    with open(filename, 'r') as f:
+        for i,line in enumerate(f):
+            sstring = line.split()
+            if i==0:
+                N = int(sstring[0])
+                atomsinfo = np.ones((N,6))
+            elif i > 1:
+                
+                atomsinfo[count,0] = int(sstring[0])
+                atomsinfo[count,1] = 1
+                atomsinfo[count,2] = int(sstring[1])
+                atomsinfo[count,3] = float(sstring[2])
+                atomsinfo[count,4] = float(sstring[3])
+                atomsinfo[count,5] = float(sstring[4])
+                count = count + 1
+
+    xlo = min(atomsinfo[:,3]) - 5
+    xhi = max(atomsinfo[:,3]) + 5
+    ylo = min(atomsinfo[:,4]) - 5
+    yhi = max(atomsinfo[:,4]) + 5
+    zlo = min(atomsinfo[:,5]) - 5
+    zhi = max(atomsinfo[:,5]) + 5
+
+    return atomsinfo, xlo, xhi, ylo, yhi, zlo, zhi
+
+def main():
+    #readlammpsbondsPPctypes("TrialInfa1iPPbonds.data", "TrialInfa1iPPCtype.data")
+    atomsinfo, xlo, xhi, ylo, yhi, zlo, zhi = setupInfiniteSystem("TrialInfa1iPP.xyz", 1, 1, 10)
+    
+    msc.writelammpsdatajustatoms("TrialInfa1iPP.data",[xlo,xhi,ylo,yhi,zlo,zhi], [15], len(atomsinfo), atomsinfo)
+    #atomsinfo, xlo, xhi, ylo, yhi, zlo, zhi = readxyz("custompp-iso.xyz")
+    #msc.writelammpsdatajustatoms("custompp-iso.data", [xlo, xhi, ylo, yhi, zlo, zhi], [14, 1], len(atomsinfo), atomsinfo)
+main()
